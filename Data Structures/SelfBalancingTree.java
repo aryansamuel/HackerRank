7f574976ddce7fbfe4a71de4c1f4de0f4e106050
Aryan Samuel <aryan.samuel0596@gmail.com>
2017-01-24 02:31:40 -0800

    Balanced Trees
    Added solution to Self-balancing tree problem.

---
+++ Data Structures/SelfBalancingTree.java
@@ -0,0 +1,75 @@
+/*
+Due to the complexity of the question, instead of copy pasting it, I have provided a link:
+https://www.hackerrank.com/challenges/self-balancing-tree
+*/
+
+   /* Class node is defined as :
+    class Node 
+       int val;   //Value
+       int ht;      //Height
+       Node left;   //Left child
+       Node right;   //Right child
+   */
+
+    static Node insert(Node root, int val) {
+        if (root == null) {
+            Node N = new Node();
+            N.val = val;
+            N.ht = 0;
+            return N;
+        }
+       
+        if (val < root.val) root.left = insert(root.left, val);
+        else root.right = insert(root.right, val);
+       
+        root = rotate(root);
+        return root;
+    }
+
+    private static Node rotate(Node root) {
+        int bf = balFac(root);
+        
+        if (bf == 2) {
+            if (balFac(root.left) == -1) root.left = rotateLeft(root.left);
+            return rotateRight(root);
+        } 
+        if (bf == -2) {
+            if (balFac(root.right) == 1) root.right = rotateRight(root.right);
+            return rotateLeft(root);
+        }
+        
+        height(root);
+        return root;
+    }
+
+    private static Node rotateLeft(Node root) {
+        Node N = root.right;
+        root.right = root.right.left;
+        N.left = root;
+        height(root);
+        height(N);
+        return(N);
+    }
+    
+    private static Node rotateRight(Node root) {
+        Node N = root.left;
+        root.left = root.left.right;
+        N.right = root;
+        height(N);
+        height(root);
+        return(N);
+    }
+
+    private static int balFac(Node root) {
+        int leftH = -1, rightH = -1;
+        if (root.left != null) leftH = root.left.ht;
+        if (root.right != null) rightH = root.right.ht;
+        return leftH - rightH;
+    }
+ 
+    private static void height(Node root) {
+        int leftH = -1, rightH = -1;
+        if (root.left != null) leftH = root.left.ht;
+        if (root.right != null) rightH = root.right.ht;
+        root.ht = 1 + Math.max(leftH, rightH);
+    }
