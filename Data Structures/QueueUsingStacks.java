b36674faab41befb202c8d18064ee67b180e57fe
Aryan Samuel <aryan.samuel0596@gmail.com>
2017-01-25 01:39:35 -0800

    Queues
    Added solution to Queue using two stacks problem.

---
+++ Data Structures/QueueUsingStacks.java
@@ -0,0 +1,50 @@
+/*
+Due to the complexity of the question, insted of copy pasting it, I have provided a link:
+https://www.hackerrank.com/challenges/queue-using-two-stacks
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        Queue<Integer> Q = new Queue<Integer>();
+        
+        for (int i=0; i<q; i++) {
+            int type = in.nextInt();
+            if (type == 1) Q.enqueue(in.nextInt());
+            else if (type == 2) Q.dequeue();
+            else System.out.println(Q.peek());
+        }
+    }
+    
+}
+
+class Queue<type> {
+    Stack<type> S1 = new Stack<type>();
+    Stack<type> S2 = new Stack<type>();
+    
+    void enqueue(type data) {
+        S1.push(data);
+    }
+    
+    type dequeue() {
+        transfer();
+        return S2.pop();
+    }
+    
+    type peek() {
+        transfer();
+        return S2.peek();
+    }
+    
+    void transfer() {
+        if (S2.isEmpty()) {
+            while (!S1.isEmpty()) S2.push(S1.pop());
+        }
+    }
+    
+}
